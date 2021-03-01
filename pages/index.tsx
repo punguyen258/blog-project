be08f7bd5eac60d0360291a56d1da971bbb0c3c9
punguyen258 <n.phuong2508@gmail.com>
2021-03-01 11:01:01 +0700

    Post detail page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,60 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { fetchEntries } from '../utils/contentfulPosts'
+import styles from '../styles/Home.module.css'
+import Post from '../components/Post'
+import Typed from 'react-typed';
+import { Container, Row } from 'react-bootstrap';
+interface AppProps {
+  posts?: any;
+}
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+const HomePage: React.FC<AppProps> = ({ posts }) => (
+  <div className="homepage">
+    <div className={styles.header}>
+      <img src="/logo.png" style={{ width: '300px' }} />
+    </div>
+    <div className="banner">
+      <img src="/banner_2.jpg" style={{ width: '100%' }} />
+      <Typed
+        className={styles.typing}
+        strings={['Welcome to Photo Blog.']}
+        typeSpeed={100}
+        backSpeed={100}
+        loop
+      />
+    </div>
+    <div className={styles.postList}>
+      <Container>
+        <Row>
+          {posts.map((post, index) => {
+            return (
+              <Post key={index}
+                id={post.id}
+                publishDate={post.publishDate}
+                title={post.title}
+                description={post.description}
+                // image={post.fields}
+                author={post.author}
+                link={`/post/${post.slug}/`}
+              />
+            )
+          })}
+        </Row>
+      </Container>
+    </div>
+  </div>
 )
 
-export default IndexPage
+export const getStaticProps = async () => {
+  const res = await fetchEntries()
+  const posts = await res.map((post) => {
+    return post.fields
+  })
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+export default HomePage
---
+++ pages/post/[slug].tsx
@@ -0,0 +1,35 @@
+// import { useRouter } from 'next/router'
+import DetailPost from '../../components/DetailPost'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { Post } from '../../interfaces'
+import { fetchEntries } from '../../utils/contentfulPosts'
+
+type Props = {
+  item?: Post;
+}
+
+const StaticPropsDetail = ({ item }: Props) => {
+  return (
+    item && <DetailPost item={item} />
+  )
+}
+export const getStaticPaths: GetStaticPaths = async () => {
+  const res = await fetchEntries()
+  const posts = await res.map((post) => {
+    return post.fields
+  })
+  const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }))
+  return { paths, fallback: false }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const res = await fetchEntries()
+  console.log(res)
+  const slug = params?.slug
+  const item = res.find((post) => post.fields.slug === String(slug))
+  return { props: { item } }
+}
+export default StaticPropsDetail
+
