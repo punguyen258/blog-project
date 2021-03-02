c74e840c8a531d101f487e4c26892c1cdb5a31b6
punguyen258 <n.phuong2508@gmail.com>
2021-03-02 13:58:49 +0700

    Pagination list in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
+import { useState, useEffect } from 'react';
 import { fetchEntries } from '../utils/contentfulPosts'
 import styles from '../styles/Home.module.css'
 import Post from '../components/Post'
 import Typed from 'react-typed';
 import { Container, Row } from 'react-bootstrap';
-import Pagination from 'next-pagination'
+import { Pagination } from 'antd';
 import Link from 'next/link';
+import "antd/dist/antd.css";
 interface AppProps {
   posts?: any;
 }
 
 const HomePage: React.FC<AppProps> = ({ posts }) => {
+  const pageSize = 8;
+  const [minIndex, setMinIndex] = useState(0);
+  const [maxIndex, setMaxIndex] = useState(pageSize);
+  const handleChangePage = (page) => {
+    setMinIndex((page - 1) * pageSize);
+    setMaxIndex(page * pageSize);
+  }
+  useEffect(() => {
+    handleChangePage(1)
+  }, [])
+
   return (
     <div className="homepage">
       <div className={styles.header}>
@@ -27,28 +40,32 @@ const HomePage: React.FC<AppProps> = ({ posts }) => {
           loop
         />
       </div>
+
       <div className={styles.postList}>
         <Container>
           <Row>
-            {posts.map((post, index) => {
-              const urlImage = post.heroImage.fields.file.url
-              return (
-                <>
-                  <Post key={index}
-                    publishDate={post.publishDate}
-                    title={post.title}
-                    description={post.description}
-                    image={urlImage}
-                    author={post.author}
-                    link={`/post/${post.slug}/`}
-                  />
-                </>
+            {posts.map((post, index) =>
+              index >= minIndex && index < maxIndex && (
+                <Post key={index}
+                  publishDate={post.publishDate}
+                  title={post.title}
+                  description={post.description}
+                  image={post.heroImage.fields.file.url}
+                  author={post.author}
+                  link={`/post/${post.slug}/`}
+                />
               )
-            })}
+            )}
           </Row>
         </Container>
       </div>
-      <Pagination />
+
+      <Pagination className={styles.pagination}
+        defaultCurrent={1}
+        total={posts.length}
+        pageSize={pageSize}
+        onChange={handleChangePage}
+      />
     </div>
   )
 }
