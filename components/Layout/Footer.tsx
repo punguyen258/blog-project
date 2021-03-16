ac4781f1b39a31f843a85176291f5d7ebf79b23f
punguyen258 <n.phuong2508@gmail.com>
2021-03-16 08:29:31 +0700

    Add layout footer

---
+++ components/Layout/Footer.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode } from 'react'
+import Link from 'next/link'
+import { Container, Row } from 'react-bootstrap'
+import styles from '../../styles/Layout.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(fab, far, fas);
+
+const Footer = () => (
+  <div className={styles.footerDark}>
+    <footer>
+      <Container>
+        <Row>
+          <div className="col-md-6 item text">
+            <h3 className={styles.titleItem}>Introduction</h3>
+            <i>Hanoi’s landscape is both ancient and modern. Hanoi will give you a taste for famous dishes. Hanoi always welcome you as your home.</i>
+          </div>
+          <div className="offset-md-1 col-md-5 item">
+            <h3 className={styles.titleItem}>Contact</h3>
+            <p className={styles.mb0}>Phuong Nguyen</p>
+            <p className={styles.mb0}><FontAwesomeIcon icon={["far", "envelope"]} className={styles.iconSmall} />n.phuong2508@gmail.com</p>
+            <p className={styles.mb0}><FontAwesomeIcon icon={["fas", "phone"]} className={styles.iconSmall} />0123456789</p>
+            <p className={styles.mb0}><FontAwesomeIcon icon={["fas", "map-marker-alt"]} className={styles.iconSmall} />Ba Dinh, Ha Noi, Viet Nam</p>
+          </div>
+          <div className={styles.social}>
+            <Link href="/"><a className={styles.linkSocial}><FontAwesomeIcon icon={["fab", "facebook"]} className={styles.icon} /></a></Link>
+            <Link href="/"><a className={styles.linkSocial}><FontAwesomeIcon icon={["fab", "twitter"]} className={styles.icon} /></a></Link>
+            <Link href="/"><a className={styles.linkSocial}><FontAwesomeIcon icon={["fab", "instagram"]} className={styles.icon} /></a></Link>
+            <Link href="/"><a className={styles.linkSocial}><FontAwesomeIcon icon={["fab", "telegram"]} className={styles.icon} /></a></Link>
+          </div>
+        </Row>
+        <p className={styles.copyright}>Phương NH © 2021</p>
+      </Container>
+    </footer>
+  </div>
+)
+
+export default Footer
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Typed from 'react-typed';
 import { Container, Row } from 'react-bootstrap';
 import { Pagination } from 'antd';
 import Link from 'next/link';
-import "antd/dist/antd.css";
+import Footer from '../components/Layout/Footer';
 interface AppProps {
   posts?: any;
 }
@@ -50,7 +50,7 @@ const HomePage: React.FC<AppProps> = ({ posts }) => {
           You should also try its delicious street foods such as: “Pho“, “Bun Cha”, “Banh Mi Pate” or “Cha ca”. The weather in Hanoi is pleasant.
           I really like the spring and the autumn so much because of good weather and beautiful color of trees.
           More and more foreigners visit Hanoi every year, so what are you waiting for? Join us and discover it now!
-        </div>
+          </div>
       </div>
 
       <div className={styles.postList}>
@@ -79,36 +79,7 @@ const HomePage: React.FC<AppProps> = ({ posts }) => {
         onChange={handleChangePage}
       />
 
-      <div className="footer-dark">
-        <footer>
-          <Container>
-            <Row>
-              <div className="col-sm-6 col-md-3 item">
-                <h3>Services</h3>
-                <ul>
-                  <li><a href="#">Web design</a></li>
-                  <li><a href="#">Development</a></li>
-                  <li><a href="#">Hosting</a></li>
-                </ul>
-              </div>
-              <div className="col-sm-6 col-md-3 item">
-                <h3>About</h3>
-                <ul>
-                  <li><a href="#">Company</a></li>
-                  <li><a href="#">Team</a></li>
-                  <li><a href="#">Careers</a></li>
-                </ul>
-              </div>
-              <div className="col-md-6 item text">
-                <h3>Company Name</h3>
-                <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
-              </div>
-              <div className="col item social"><a href="#"><i className="icon ion-social-facebook"></i></a><a href="#"><i className="icon ion-social-twitter"></i></a><a href="#"><i className="icon ion-social-snapchat"></i></a><a href="#"><i className="icon ion-social-instagram"></i></a></div>
-            </Row>
-            <p className="copyright">Company Name © 2018</p>
-          </Container>
-        </footer>
-      </div>
+      <Footer />
     </div>
   )
 }
